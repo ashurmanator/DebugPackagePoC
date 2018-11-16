368eee3c4637186a5b7d37be16088b450f53af6b
Andrew Shurman <ashurman@asu.edu>
2018-11-16 14:08:04 -0700

    Initial Commit
    Init commit

---
+++ DebugPackagePoC/SimpleFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace DebugPackagePoC
+{
+    public class SimpleFunctions
+    {
+        public int MultiplyIntegers(int a, int b)
+        {
+            int result = 0;
+            for (int i = 0; i < a; i++)
+            {
+                result += b;
+            }
+
+            return result;
+        }
+    }
+}
