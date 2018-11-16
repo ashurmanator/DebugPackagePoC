b3916007ffd5c26433a19a3f3ef6c994b095f09d
Andrew Shurman <ashurman@asu.edu>
2018-11-16 14:22:05 -0700

    Redo init commit

---
+++ DebugPackagePoC/SimpleFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DebugPackagePoC
+{
+    public class SimpleFunctions
+    {
+        public int MultiplyIntegers(int a, int b)
+        {
+            int result = 0;
+            for (int i = 0; i < a; i++)
+            {
+                result += b;
+            }
+
+            return result;
+        }
+    }
+}
